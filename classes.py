34a42109711afc6c934f4f87e2b8f43ddaba286f
Nataly Merezhuk <nmerezhuk@digitalglobalsystems.com>
2021-08-24 15:37:49 -0400

    Working on making dicitonary entries into objects for further processing.

--- classes.py
+++ classes.py
@@ -4,12 +4,14 @@ class File(object):
 
     def __init__(self, error_dictionary):
         for key in error_dictionary:
-            key = self.clean(key)
-            print(key)
+            print(f'From File class: {error_dictionary[key]}')
+            for value in error_dictionary[key]:
+                print(error_dictionary[key][value])
+            # key = self.clean(key)
+            # print(key)
             time.sleep(5)
 
     def clean(self, path: str) -> str:
         clean_path = path.split(':', 1)
-        print(clean_path)
-        print(type(clean_path))
+        clean_path = clean_path[0]
         return clean_path
